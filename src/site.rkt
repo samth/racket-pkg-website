a6eebdfb7290ad3d9ba7b71778d1fe1acdf9b35b
Sam Tobin-Hochstadt <samth@iu.edu>
2026-03-05 16:06:21 -0500

    Assign user IDs at login time
    Call ensure-user-id! in create-session-after-authentication-success!
    so that every user gets a UUID v4 assigned on their first login.

--- src/site.rkt
+++ src/site.rkt
@@ -380,6 +380,7 @@
                             ,(form-group 4 5 (primary-button "Log in"))))))))
 
 (define (create-session-after-authentication-success! email)
+  (ensure-user-id! email)
   (create-session! email
                    #:curator? (and (curation-administrator? email) #t)
                    #:superuser? (and (superuser? email) #t)))
